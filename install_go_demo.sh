d862c6fecf439b02c55d72ff89239e356c1038a4
Stephen Chu <13208971+stephenchu530@users.noreply.github.com>
2019-07-24 12:01:54 -0700

    Update install_go_demo.sh

--- install_go_demo.sh
+++ install_go_demo.sh
@@ -11,8 +11,11 @@ curl -O https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 tar xvf go1.10.3.linux-amd64.tar.gz
 chown -R root:root ./go
 mv go /usr/local
-export GOPATH=$HOME/work
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+mkdir Projects
+mkdir Projects/Proj1
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/Projects/Proj1
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 rm go1.10.3.linux-amd64.tar.gz
 echo "Done"
 echo "Install Fragmenta..."
