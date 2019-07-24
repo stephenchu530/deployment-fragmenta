cbaea2b1925877a91576d690694c50dcc6d9be92
Stephen Chu <13208971+stephenchu530@users.noreply.github.com>
2019-07-24 13:47:27 -0700

    Update install_go_demo.sh

--- install_go_demo.sh
+++ install_go_demo.sh
@@ -7,16 +7,14 @@ echo "Installing Postgresql..."
 sudo apt-get -y install postgresql postgresql-contrib
 echo "Done"
 echo "Installing GO..."
+echo "export GOPATH="${HOME}/.go"" >> ~/.profile
+echo "export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin" >> ~/.profile
+test -d "${GOPATH}" || mkdir "${GOPATH}"
+test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
 curl -O https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 tar xvf go1.10.3.linux-amd64.tar.gz
 sudo chown -R root:root ./go
 sudo mv go /usr/local
-mkdir Projects
-mkdir Projects/Proj1
-echo "export GOROOT=/usr/local/go" >> ~/.profile
-echo "export GOPATH=$HOME/Projects/Proj1" >> ~/.profile
-echo "export PATH=$GOPATH/bin:$GOROOT/bin:/usr/local/go/bin:$PATH" >> ~/.profile
-source ~/.profile
 rm go1.10.3.linux-amd64.tar.gz
 echo "Done"
 echo "Install Fragmenta..."
